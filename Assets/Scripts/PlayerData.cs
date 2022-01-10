e0dbac968b8940631c9fd458738f95698c2f93f5
SAM HARRIS <SH248212@falmouth.ac.uk>
2022-01-10 09:36:00 +0000

    Added the load function

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -16,7 +16,7 @@ public class PlayerData
 
         position = new float[3];
         position[0] = player.transform.position.x;
-        position[1] = player.transform.position.x;
-        position[2] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
     }
 }
